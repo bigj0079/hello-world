025ea50fd7d2ec982d7283a98979f6306fc48957
bigj0079 <bigj0079@hotmail.com>
2025-05-20 01:21:50 +0900

    daily commit

--- Hello.py
+++ Hello.py
@@ -1,1 +1,17 @@
+# very first python program
 print("Hello, World!")
+# printing the type of different data types
+print(type("Hello, World!"))
+print(type(1))
+print(type(1.0))
+
+fruit = ["apple", "banana", "cherry"]
+print(type(fruit))
+print(type(fruit[0]))
+print(fruit)
+print(fruit[0])
+print(fruit[0:1])
+print(fruit[0], fruit[1])
+print(
+    fruit[0:10]
+)  # out of range will not give error, it will just return the list till the end
